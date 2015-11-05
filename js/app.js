f7381d8ceb94f8597a03962401cfa3f7c19b16b7
Josh Graber <jgraber@renovo1.com>
2015-11-05 11:13:28 -0500

    Add a module for services.

--- js/app.js
+++ js/app.js
@@ -4,10 +4,12 @@ var angular = require('angular');
 
 var behaviorsModule = require('./behaviors/behaviors.module');
 var controllersModule = require('./controllers/controllers.module');
+var servicesModule = require('./services/services.module');
 
 exports.moduleName = 'exp-ang';
 
 angular.module(exports.moduleName, [
 	behaviorsModule.moduleName,
 	controllersModule.moduleName,
+	servicesModule.moduleName,
 ]);
---
+++ js/services/services.module.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var angular = require('angular');
+
+exports.moduleName = 'exp-ang.services';
+
+angular.module(exports.moduleName, [
+]);
