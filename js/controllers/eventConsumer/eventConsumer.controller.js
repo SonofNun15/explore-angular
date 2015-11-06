1878ca507f186df055a002ff984b0c9c3692459e
Josh Graber <jgraber@renovo1.com>
2015-11-06 08:45:02 -0500

    Add a consumer for the custom stream and added public interfaces declaration for both stream consumers.

--- js/controllers/eventConsumer/eventConsumer.controller.js
+++ js/controllers/eventConsumer/eventConsumer.controller.js
@@ -1,15 +1,33 @@
 'use strict';
 
 var angular = require('angular');
+var customStream = require('../../services/customStream/customStream.service');
 
 exports.moduleName = 'exp-ang.controllers.eventConsumer';
 exports.controllerName = 'EventConsumerController';
 
-EventConsumerController.$inject = ['$scope'];
-function EventConsumerController($scope) {
+EventConsumerController.$inject = ['$scope', customStream.serviceName];
+function EventConsumerController($scope, customStream) {
 	var self = this;
 
+	// --- public interface ---
+	// Event
+	self.hitCount = 0;
+	self.xPos = null;
+	self.finished = false;
+	self.eventError = null;
+
+	// Custom
+	self.customCurrentValue = null;
+	self.customFinished = false;
+	self.customError = null;
+
+	self.finishCustom = customStream.end;
+	self.generateCustomError = customStream.error;
+
+	// --- initialize ---
 	consumeEventStream();
+	consumeCustomStream();
 
 	function consumeEventStream() {
 		var oldSubscription;
@@ -30,13 +48,28 @@ function EventConsumerController($scope) {
 			oldSubscription = xCoordStream.subscribe(function(data) {
 				$scope.$apply(function() { self.hitCount++; self.xPos = data.position; });
 			}, function(error) {
-				$scope.$apply(function() { self.error = error; });
+				$scope.$apply(function() { self.eventError = error; });
 			}, function() {
 				$scope.$apply(function() { self.finished = true; });
 			});
 		});
 	}
+
+	function consumeCustomStream() {
+		// Use controlled() to cause stream to only supply values on request
+		var customSourceStream = customStream.get();
+
+		customSourceStream.subscribe(function(value) {
+			$scope.$apply(function() {
+				self.currentCustomValue = value;
+			});
+		}, function(error) {
+			self.customError = error;
+		}, function() {
+			self.customFinished = true;
+		});
+	}
 }
 
-angular.module(exports.moduleName, [])
+angular.module(exports.moduleName, [customStream.moduleName])
 	.controller(exports.controllerName, EventConsumerController);
