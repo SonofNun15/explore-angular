c1c6fb9f56d2f70425a5fca93ddd0b7e2826d2c7
Josh Graber <jgraber@renovo1.com>
2015-12-16 09:38:32 -0500

    Add a test to verify that the controller calls the calculator service properly.

--- js/helloWorld/helloWorld.controller.tests.js
+++ js/helloWorld/helloWorld.controller.tests.js
@@ -27,9 +27,26 @@ describe('helloWorld', function() {
 		}]);
 	});
 
-	it ('should have a message', function() {
+	it('should have a message', function() {
 		var controller = buildController();
 		expect(controller.message).to.equal('Hello Angular explorers!')
+	});
+
+	it('should calculate the result of the operands and operator', function() {
+		var controller = buildController();
+
+		controller.operand1 = 5;
+		controller.operand2 = 10;
+		controller.operator = '*';
+		mockCalculator.calculateResult = 'mocked result';
+
+		controller.equals();
+
+		expect(controller.result).to.equal('mocked result');
+		expect(controller.error).to.be.null;
+
+		sinon.assert.calledOnce(mockCalculator.calculate);
+		sinon.assert.calledWith(mockCalculator.calculate, '*', 5, 10);
 	});
 
 	function buildController() {
